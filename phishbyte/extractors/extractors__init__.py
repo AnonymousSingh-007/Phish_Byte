1781d7ae7faafcfb9a3b1e3116cd0cfa7d6df199
Samratth Singh <samratthsingh@proton.me>
2026-07-24 08:17:12 +0530

    Add BDI and TF-IDF imports to extractors module: update __all__ for feature inclusion

--- phishbyte/extractors/extractors__init__.py
+++ phishbyte/extractors/extractors__init__.py
@@ -1,6 +1,9 @@
-from phishbyte.extractors.domain  import score_domain
-from phishbyte.extractors.urls    import score_urls
-from phishbyte.extractors.spf     import score_spf
-from phishbyte.extractors.subject import score_subject
+from phishbyte.extractors.domain         import score_domain
+from phishbyte.extractors.urls           import score_urls
+from phishbyte.extractors.spf            import score_spf
+from phishbyte.extractors.subject        import score_subject
+from phishbyte.extractors.bdi            import score_bdi
+from phishbyte.extractors.tfidf_features import TFIDFVocab
 
-__all__ = ["score_domain", "score_urls", "score_spf", "score_subject"]
+__all__ = ["score_domain","score_urls","score_spf",
+           "score_subject","score_bdi","TFIDFVocab"]
